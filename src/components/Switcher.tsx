95ae09a86c3fc541553f7a593edf73c9f3d4f002
Larry Ntori <l.ntori@generisdevelopers.com>
2023-03-16 13:47:11 +0000

    new features

--- src/components/Switcher.tsx
+++ src/components/Switcher.tsx
@@ -3,24 +3,24 @@ import { DarkModeSwitch } from "react-toggle-dark-mode";
 import Darkmode from "./DarkMode";
 
 export default function Switcher() {
-	const {colorTheme, setTheme} = Darkmode();
-	const [darkSide, setDarkSide] = useState(
-		colorTheme === "light" ? true : false
-	);
+  const { colorTheme, setTheme } = Darkmode();
+  const [darkSide, setDarkSide] = useState(
+    colorTheme === "light" ? true : false
+  );
 
-	const toggleDarkMode = (checked) => {
-		setTheme(colorTheme);
-		setDarkSide(checked);
-	};
+  const toggleDarkMode = (checked) => {
+    setTheme(colorTheme);
+    setDarkSide(checked);
+  };
 
-	return (
-		<>
-			<DarkModeSwitch
-				// style={{ marginBottom: "2rem" }}
-				checked={darkSide}
-				onChange={toggleDarkMode}
-				size={30}
-			/>
-		</>
-	);
+  return (
+    <>
+      <DarkModeSwitch
+        // style={{ marginBottom: "2rem" }}
+        checked={darkSide}
+        onChange={toggleDarkMode}
+        size={30}
+      />
+    </>
+  );
 }
